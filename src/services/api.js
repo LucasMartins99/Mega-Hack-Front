8cfcf9bd118530ff942761caafddc9a2f9d8d99b
fabio lucas lima <fabiolucaslima@MacBook-Air-de-fabio.local>
2020-04-08 00:45:08 -0300

    Mudando banco de daos para umbler 2

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://megahackdigital.com:3000',
+  baseURL: 'http://megahackdigital-com.umbler.net:3000',
 });
 export default api;
